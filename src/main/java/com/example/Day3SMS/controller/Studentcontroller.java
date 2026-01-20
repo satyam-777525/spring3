fca67e6e695c9ec8b943b83bacde8cc7ff4a9677
satyam-777525 <sv1061211@gmail.com>
2026-01-20 15:42:00 +0530

    mongodb connection

--- src/main/java/com/example/Day3SMS/controller/Studentcontroller.java
+++ src/main/java/com/example/Day3SMS/controller/Studentcontroller.java
@@ -1,12 +1,28 @@
 package com.example.Day3SMS.controller;
 
+import com.example.Day3SMS.model.StudentModel;
+import com.example.Day3SMS.service.StudentService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Studentcontroller {
-    @GetMapping("/")
-    public String student(){
-        return "home page";
+  private final StudentService service;
+
+    public Studentcontroller(StudentService service) {
+        this.service = service;
+    }
+    @GetMapping("/test")
+    public String test(){
+        return "API is working";
+    }
+
+    // create function API
+    @PostMapping("/add-student")
+    public StudentModel addStudent(@RequestBody StudentModel student){
+//        System.out.println(student);
+        return service.addStudent(student);
     }
 }
---
+++ src/main/java/com/example/Day3SMS/service/StudentService.java
@@ -0,0 +1,22 @@
+package com.example.Day3SMS.service;
+
+import com.example.Day3SMS.model.StudentModel;
+import com.example.Day3SMS.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository repository;
+
+    public StudentService(StudentRepository repository) {
+        this.repository = repository;
+    }
+
+    // CREATE Student
+    public StudentModel addStudent(StudentModel student) {
+        return repository.save(student);
+    }
+
+
+}
