9ae2b6d506cf0ee1146e3d1b3910bf52b1a29b30
satyam-777525 <sv1061211@gmail.com>
2026-01-27 15:29:21 +0530

    Initial commit

--- src/main/java/com/example/Day3SMS/controller/Studentcontroller.java
+++ src/main/java/com/example/Day3SMS/controller/Studentcontroller.java
@@ -2,10 +2,9 @@ package com.example.Day3SMS.controller;
 
 import com.example.Day3SMS.model.StudentModel;
 import com.example.Day3SMS.service.StudentService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class Studentcontroller {
@@ -25,4 +24,24 @@ public class Studentcontroller {
 //        System.out.println(student);
         return service.addStudent(student);
     }
+
+    @GetMapping("/students")
+    public List<StudentModel> getStudents(){
+        return service.getStudents();
+    }
+    @PutMapping("/update/{id}")
+    public StudentModel updateStudent(
+            @PathVariable String id,
+            @RequestBody StudentModel student) {
+        return service.updateStudent(id, student);
+    }
+
+    @DeleteMapping("/students/{id}")
+    public String deleteStudent(@PathVariable String id) {
+        return service.deleteStudent(id);
+    }
+
+
+
+
 }
--- src/main/java/com/example/Day3SMS/service/StudentService.java
+++ src/main/java/com/example/Day3SMS/service/StudentService.java
@@ -2,7 +2,10 @@ package com.example.Day3SMS.service;
 
 import com.example.Day3SMS.model.StudentModel;
 import com.example.Day3SMS.repository.StudentRepository;
+import lombok.Data;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StudentService {
@@ -17,6 +20,30 @@ public class StudentService {
     public StudentModel addStudent(StudentModel student) {
         return repository.save(student);
     }
+
+    //Display Students
+    public List<StudentModel> getStudents(){
+        return repository.findAll();
+    }
+
+    //Update Students
+    public StudentModel updateStudent(String id,StudentModel student){
+        StudentModel existingStudent=repository.findById(id)
+                .orElseThrow(()->new RuntimeException("No student found"));
+        existingStudent.setName(student.getName());
+        existingStudent.setAge(student.getAge());
+        existingStudent.setEmail(student.getEmail());
+
+        return repository.save(existingStudent);
+    }
+    // delete student
+    public String deleteStudent(String id) {
+        StudentModel existingStudent = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("No student found"));
+        repository.delete(existingStudent);
+        return "Student deleted successfully";
+    }
+
 
 
 }
